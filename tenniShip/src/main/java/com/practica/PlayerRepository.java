6a2d1e3fa2e04d41a6734425f9c35d9951be5004
IvanFernandezLlorente <ivi_7fu@hotmail.com>
2020-02-12 12:54:32 +0100

    Format Code and rename files -Format code html and css -PlayerRepository deleted

--- tenniShip/src/main/java/com/practica/PlayerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.practica;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.model.Player;;
-
-public interface PlayerRepository extends JpaRepository<Player, Long> {
-
-}
