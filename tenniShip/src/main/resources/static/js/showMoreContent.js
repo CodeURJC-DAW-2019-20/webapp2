778ed626527c9fe811b37417818996ea647ce645
Diego <pascual.diego7@gmail.com>
2020-02-23 11:19:13 +0100

    Bugs fixed, added show more buttons function
    Some bugs fixed in which the graphics started malfunctioning, added show more button for the matches of any team and tournaments, fixed the footers adaptability to the webs where it created conflict, prepare it for the changed of the height caused by loading more components dynamically

---
+++ tenniShip/src/main/resources/static/js/showMoreContent.js
@@ -0,0 +1,30 @@
+//Slice method for content in tournaments section
+$('.displayTournamentsHidden').slice(0,3).show();
+
+$('#loadContentButtonTournament').on('click',async function(){
+    $('.displayTournamentsHidden:hidden').slice(0,3).slideDown();
+    await sleep(250);
+    windowResize();
+});
+
+$('.displayMatchesHidden').slice(0,2).show();
+
+$('#loadContentButtonMatches').on('click',async function(){
+    $('.displayMatchesHidden:hidden').slice(0,1).slideDown();
+    await sleep(250);
+    windowResize();
+});
+
+
+function windowResize() {
+    if($(window).height() >= ($('body').height() + 92)) {
+        $('footer').addClass('shortContent');
+    }
+    else {
+        $('footer').removeClass('shortContent');
+    }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
