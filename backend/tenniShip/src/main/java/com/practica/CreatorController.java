a34d3f410e6c9845898497e5f9b1fd2d595d321b
MarcosVillacanas <marcosvillacanas@icloud.com>
2020-03-03 17:23:34 +0100

    CreatorRest Controller Done
    -Now we use Services!!! Functional Services.
    -Create a Tournament by Rest is available
    
    Co-Authored-By: ivanfernandezllorente <ivanfernandezllorente@users.noreply.github.com>

--- backend/tenniShip/src/main/java/com/practica/CreatorController.java
+++ backend/tenniShip/src/main/java/com/practica/CreatorController.java
@@ -36,9 +36,9 @@ public class CreatorController {
 
 	@Autowired
 	private UserComponent userComponent;
-
+	
 	@Autowired
-	private MatchRepository matchRepository;
+	private CreatorService creatorService;
 
 	private Tournament finalTournament = new Tournament();
 	private String[] teamList = new String[18];
@@ -179,24 +179,10 @@ public class CreatorController {
 
 		model.addAttribute("tourFinal", finalTournament);
 		model.addAttribute("next1", !finalTournament.getName().isEmpty());
-		check(model, team1, 1);
-		check(model, team2, 2);
-		check(model, team3, 3);
-		check(model, team4, 4);
-		check(model, team5, 5);
-		check(model, team6, 6);
-		check(model, team7, 7);
-		check(model, team8, 8);
-		check(model, team9, 9);
-		check(model, team10, 10);
-		check(model, team11, 11);
-		check(model, team12, 12);
-		check(model, team13, 13);
-		check(model, team14, 14);
-		check(model, team15, 15);
-		check(model, team16, 16);
-		check(model, team17, 17);
-		check(model, team18, 18);
+		
+		for (int i = 1; i < 19; i++) {
+			check(model, String.format("team%d", i), i);
+		}
 		exist(model, teamList);
 
 		model.addAttribute("next2", numberTeams == 18);
@@ -232,7 +218,7 @@ public class CreatorController {
 			Optional<Team> team = teamRepository.findById(x);
 			teamListFinal.add(team.get());
 		}
-		raffleTeamsCreateMatches(finalTournament, teamListFinal);
+		creatorService.raffleTeamsCreateMatches(finalTournament, teamListFinal);
 
 		/* Adding a list of all teams for the autocomplete */
 		model.addAttribute("teamNames", teamNames());
@@ -257,102 +243,6 @@ public class CreatorController {
 			return "redirect:/TenniShip/SignIn";
 		}
 
-	}
-
-	public void raffleTeamsCreateMatches(Tournament tournament, List<Team> teams) {
-
-		Collections.shuffle(teams);
-
-		Match m1 = new Match(0, 0, "A");
-		m1.setTeam1(teams.get(0));
-		m1.setTeam2(teams.get(2));
-		m1.setTournament(tournament);
-		matchRepository.save(m1);
-		Match m2 = new Match(0, 0, "A");
-		m2.setTeam1(teams.get(2));
-		m2.setTeam2(teams.get(1));
-		m2.setTournament(tournament);
-		matchRepository.save(m2);
-		Match m3 = new Match(0, 0, "A");
-		m3.setTeam1(teams.get(1));
-		m3.setTeam2(teams.get(0));
-		m3.setTournament(tournament);
-		matchRepository.save(m3);
-		Match m4 = new Match(0, 0, "B");
-		m4.setTeam1(teams.get(3));
-		m4.setTeam2(teams.get(4));
-		m4.setTournament(tournament);
-		matchRepository.save(m4);
-		Match m5 = new Match(0, 0, "B");
-		m5.setTeam1(teams.get(5));
-		m5.setTeam2(teams.get(3));
-		m5.setTournament(tournament);
-		matchRepository.save(m5);
-		Match m6 = new Match(0, 0, "B");
-		m6.setTeam1(teams.get(4));
-		m6.setTeam2(teams.get(5));
-		m6.setTournament(tournament);
-		matchRepository.save(m6);
-		Match m7 = new Match(0, 0, "C");
-		m7.setTeam1(teams.get(6));
-		m7.setTeam2(teams.get(7));
-		m7.setTournament(tournament);
-		matchRepository.save(m7);
-		Match m8 = new Match(0, 0, "C");
-		m8.setTeam1(teams.get(8));
-		m8.setTeam2(teams.get(6));
-		m8.setTournament(tournament);
-		matchRepository.save(m8);
-		Match m9 = new Match(0, 0, "C");
-		m9.setTeam1(teams.get(7));
-		m9.setTeam2(teams.get(8));
-		m9.setTournament(tournament);
-		matchRepository.save(m9);
-		Match m10 = new Match(0, 0, "D");
-		m10.setTeam1(teams.get(9));
-		m10.setTeam2(teams.get(10));
-		m10.setTournament(tournament);
-		matchRepository.save(m10);
-		Match m11 = new Match(0, 0, "D");
-		m11.setTeam1(teams.get(11));
-		m11.setTeam2(teams.get(9));
-		m11.setTournament(tournament);
-		matchRepository.save(m11);
-		Match m12 = new Match(0, 0, "D");
-		m12.setTeam1(teams.get(10));
-		m12.setTeam2(teams.get(11));
-		m12.setTournament(tournament);
-		matchRepository.save(m12);
-		Match m13 = new Match(0, 0, "E");
-		m13.setTeam1(teams.get(12));
-		m13.setTeam2(teams.get(13));
-		m13.setTournament(tournament);
-		matchRepository.save(m13);
-		Match m14 = new Match(0, 0, "E");
-		m14.setTeam1(teams.get(14));
-		m14.setTeam2(teams.get(12));
-		m14.setTournament(tournament);
-		matchRepository.save(m14);
-		Match m15 = new Match(0, 0, "E");
-		m15.setTeam1(teams.get(13));
-		m15.setTeam2(teams.get(14));
-		m15.setTournament(tournament);
-		matchRepository.save(m15);
-		Match m16 = new Match(0, 0, "F");
-		m16.setTeam1(teams.get(15));
-		m16.setTeam2(teams.get(16));
-		m16.setTournament(tournament);
-		matchRepository.save(m16);
-		Match m17 = new Match(0, 0, "F");
-		m17.setTeam1(teams.get(17));
-		m17.setTeam2(teams.get(15));
-		m17.setTournament(tournament);
-		matchRepository.save(m17);
-		Match m18 = new Match(0, 0, "F");
-		m18.setTeam1(teams.get(16));
-		m18.setTeam2(teams.get(17));
-		m18.setTournament(tournament);
-		matchRepository.save(m18);
 	}
 
 }
--- backend/tenniShip/src/main/java/com/practica/CreatorRestController.java
+++ backend/tenniShip/src/main/java/com/practica/CreatorRestController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,137 +34,48 @@ public class CreatorRestController {
 
 	@Autowired
 	private UserComponent userComponent;
-
+	
 	@Autowired
-	private MatchRepository matchRepository;
+	private CreatorService creatorService;
+	
+	private static class CreatorAuxClass {
+		private String tournamentName;
+		private List<String> teams;
+		
+		public String getTournamentName() {
+			return tournamentName;
+		}
+		public List<String> getTeams() {
+			return teams;
+		}	
+	}
 
 	@PostMapping("/api/TenniShip/Creator")
-	public ResponseEntity<Tournament> createRest(@RequestParam String tournament, @RequestParam String team1,
-			@RequestParam String team2, @RequestParam String team3, @RequestParam String team4,
-			@RequestParam String team5, @RequestParam String team6, @RequestParam String team7,
-			@RequestParam String team8, @RequestParam String team9, @RequestParam String team10,
-			@RequestParam String team11, @RequestParam String team12, @RequestParam String team13,
-			@RequestParam String team14, @RequestParam String team15, @RequestParam String team16,
-			@RequestParam String team17, @RequestParam String team18) {
+	public ResponseEntity<Tournament> createRest(@RequestBody CreatorAuxClass creatorAuxObject ) {
 
 		if (userComponent.isLoggedUser()) {
 
-			if (tournamentRepository.findById(tournament).isPresent()) {
-				return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+			if (tournamentRepository.findById(creatorAuxObject.getTournamentName()).isPresent()) {
+				return new ResponseEntity<>(HttpStatus.CONFLICT);
 			} else {
 				Set<Team> teams = new HashSet<>();
 				for (int i = 1; i < 19; i++) {
-					Optional<Team> team = teamRepository.findById(String.format("team%d", i));
-					if (team.isPresent() && !(teams.contains(team.get()))) {
+					Optional<Team> team = teamRepository.findById(creatorAuxObject.getTeams().get(i-1));
+					if (team.isPresent()) { //A team is not accepted a team twice
 						teams.add(team.get());
 					} else {
-						return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+						return new ResponseEntity<>(HttpStatus.CONFLICT);
 					}
 				}
-				Tournament tournamentFinal = new Tournament(tournament);
-				raffleTeamsCreateMatches(tournamentFinal, teams.stream().collect(Collectors.toList()));
+				Tournament tournamentFinal = new Tournament(creatorAuxObject.getTournamentName());
+				tournamentRepository.save(tournamentFinal);
+				creatorService.raffleTeamsCreateMatches(tournamentFinal, teams.stream().collect(Collectors.toList()));
 				return new ResponseEntity<>(tournamentFinal,HttpStatus.OK);
 			}
 
 		} else {
-			return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
 		}
-	}
-
-	public void raffleTeamsCreateMatches(Tournament tournament, List<Team> teams) {
-
-		Collections.shuffle(teams);
-
-		Match m1 = new Match(0, 0, "A");
-		m1.setTeam1(teams.get(0));
-		m1.setTeam2(teams.get(2));
-		m1.setTournament(tournament);
-		matchRepository.save(m1);
-		Match m2 = new Match(0, 0, "A");
-		m2.setTeam1(teams.get(2));
-		m2.setTeam2(teams.get(1));
-		m2.setTournament(tournament);
-		matchRepository.save(m2);
-		Match m3 = new Match(0, 0, "A");
-		m3.setTeam1(teams.get(1));
-		m3.setTeam2(teams.get(0));
-		m3.setTournament(tournament);
-		matchRepository.save(m3);
-		Match m4 = new Match(0, 0, "B");
-		m4.setTeam1(teams.get(3));
-		m4.setTeam2(teams.get(4));
-		m4.setTournament(tournament);
-		matchRepository.save(m4);
-		Match m5 = new Match(0, 0, "B");
-		m5.setTeam1(teams.get(5));
-		m5.setTeam2(teams.get(3));
-		m5.setTournament(tournament);
-		matchRepository.save(m5);
-		Match m6 = new Match(0, 0, "B");
-		m6.setTeam1(teams.get(4));
-		m6.setTeam2(teams.get(5));
-		m6.setTournament(tournament);
-		matchRepository.save(m6);
-		Match m7 = new Match(0, 0, "C");
-		m7.setTeam1(teams.get(6));
-		m7.setTeam2(teams.get(7));
-		m7.setTournament(tournament);
-		matchRepository.save(m7);
-		Match m8 = new Match(0, 0, "C");
-		m8.setTeam1(teams.get(8));
-		m8.setTeam2(teams.get(6));
-		m8.setTournament(tournament);
-		matchRepository.save(m8);
-		Match m9 = new Match(0, 0, "C");
-		m9.setTeam1(teams.get(7));
-		m9.setTeam2(teams.get(8));
-		m9.setTournament(tournament);
-		matchRepository.save(m9);
-		Match m10 = new Match(0, 0, "D");
-		m10.setTeam1(teams.get(9));
-		m10.setTeam2(teams.get(10));
-		m10.setTournament(tournament);
-		matchRepository.save(m10);
-		Match m11 = new Match(0, 0, "D");
-		m11.setTeam1(teams.get(11));
-		m11.setTeam2(teams.get(9));
-		m11.setTournament(tournament);
-		matchRepository.save(m11);
-		Match m12 = new Match(0, 0, "D");
-		m12.setTeam1(teams.get(10));
-		m12.setTeam2(teams.get(11));
-		m12.setTournament(tournament);
-		matchRepository.save(m12);
-		Match m13 = new Match(0, 0, "E");
-		m13.setTeam1(teams.get(12));
-		m13.setTeam2(teams.get(13));
-		m13.setTournament(tournament);
-		matchRepository.save(m13);
-		Match m14 = new Match(0, 0, "E");
-		m14.setTeam1(teams.get(14));
-		m14.setTeam2(teams.get(12));
-		m14.setTournament(tournament);
-		matchRepository.save(m14);
-		Match m15 = new Match(0, 0, "E");
-		m15.setTeam1(teams.get(13));
-		m15.setTeam2(teams.get(14));
-		m15.setTournament(tournament);
-		matchRepository.save(m15);
-		Match m16 = new Match(0, 0, "F");
-		m16.setTeam1(teams.get(15));
-		m16.setTeam2(teams.get(16));
-		m16.setTournament(tournament);
-		matchRepository.save(m16);
-		Match m17 = new Match(0, 0, "F");
-		m17.setTeam1(teams.get(17));
-		m17.setTeam2(teams.get(15));
-		m17.setTournament(tournament);
-		matchRepository.save(m17);
-		Match m18 = new Match(0, 0, "F");
-		m18.setTeam1(teams.get(16));
-		m18.setTeam2(teams.get(17));
-		m18.setTournament(tournament);
-		matchRepository.save(m18);
 	}
 
 }
