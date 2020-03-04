0b2f259f4699dcfa51efab256d3f9ba04ae69a16
Varo412 <alvarora12@gmail.com>
2020-03-05 00:51:15 +0100

    TournamentCreatorRest including Pictures
    Now we can create tournament with pictures via Rest

---
+++ backend/tenniShip/src/main/java/com/practica/CreatorRestController.java
@@ -0,0 +1,133 @@
+package com.practica;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import com.practica.model.Team;
+import com.practica.model.Tournament;
+import com.practica.security.UserComponent;
+
+@RestController
+public class CreatorRestController {
+
+	@Autowired
+	private TeamRepository teamRepository;
+
+	@Autowired
+	private TournamentRepository tournamentRepository;
+
+	@Autowired
+	private ImageService imgService;
+
+	@Autowired
+	private UserComponent userComponent;
+
+	@Autowired
+	private CreatorService creatorService;
+
+	private static class CreatorAuxClass {
+		private String tournamentName;
+		private List<String> teams;
+
+		@SuppressWarnings("unused")
+		public CreatorAuxClass() {
+
+		}
+
+		public String getTournamentName() {
+			return tournamentName;
+		}
+
+		public List<String> getTeams() {
+			return teams;
+		}
+	}
+
+	private static class CreatorAuxClassToReturn {
+		interface Basic {
+		}
+
+		@JsonView(Basic.class)
+		private Tournament tournamentName;
+
+		@JsonView(Basic.class)
+		private List<Team> teams;
+		
+		@SuppressWarnings("unused")
+		public CreatorAuxClassToReturn() {
+
+		}
+
+		public CreatorAuxClassToReturn(Tournament tournamentName, List<Team> teams) {
+			this.tournamentName = tournamentName;
+			this.teams = teams;
+		}
+
+	}
+
+	interface creator extends CreatorAuxClassToReturn.Basic, Team.Basic, Tournament.Basic {
+	}
+
+	@JsonView(creator.class)
+	@PostMapping("/api/TenniShip/Creator")
+	public ResponseEntity<CreatorAuxClassToReturn> createRest(@RequestBody CreatorAuxClass creatorAuxObject) {
+
+		if (userComponent.isLoggedUser()) {
+
+			if (tournamentRepository.findById(creatorAuxObject.getTournamentName()).isPresent()) {
+				return new ResponseEntity<>(HttpStatus.CONFLICT);
+			} else {
+				Set<Team> teams = new HashSet<>();
+				for (int i = 1; i < 19; i++) {
+					Optional<Team> team = teamRepository.findById(creatorAuxObject.getTeams().get(i - 1));
+					if (team.isPresent()) { // A team is not accepted a team twice
+						teams.add(team.get());
+					} else {
+						return new ResponseEntity<>(HttpStatus.CONFLICT);
+					}
+				}
+				Tournament tournamentFinal = new Tournament(creatorAuxObject.getTournamentName());
+				tournamentRepository.save(tournamentFinal);
+				List<Team> teamList = new ArrayList<>();
+				teamList = teams.stream().collect(Collectors.toList());
+				creatorService.raffleTeamsCreateMatches(tournamentFinal, teamList);
+				CreatorAuxClassToReturn creatorAuxClassToReturn = new CreatorAuxClassToReturn(tournamentFinal,
+						teamList);
+				return new ResponseEntity<>(creatorAuxClassToReturn, HttpStatus.OK);
+			}
+
+		} else {
+			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+		}
+	}
+	
+	@PostMapping("/api/TenniShip/Tournament/{tournamentID}/image")
+	public ResponseEntity<Tournament> newTournamentImg(@PathVariable String tournamentID, @RequestParam MultipartFile imageFile)
+			throws IOException {
+		Optional<Tournament> tournament = tournamentRepository.findById(tournamentID);
+		if (tournament.isPresent()) {
+			tournament.get().setImage(true);
+			tournamentRepository.save(tournament.get());
+			imgService.saveImage("tournaments", tournament.get().getName(), imageFile);
+			return new ResponseEntity<>(HttpStatus.CREATED);
+		} else
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+
+}
