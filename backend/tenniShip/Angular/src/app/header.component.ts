6e735b5e194e06f4ebe299779767896a3bdde853
MarcosVillacanas <marcosvillacanas@icloud.com>
2020-03-17 09:19:57 +0100

    Page Bug Fixed
    White Square does not appear anymore

--- backend/tenniShip/Angular/src/app/header.component.ts
+++ backend/tenniShip/Angular/src/app/header.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   //styleUrls: ['.app.component.css']
 })
 export class HeaderComponent {
-  //title = 'spaheader';
+  title = 'spaheader';
   admin = true;
   registered = true;
 }
