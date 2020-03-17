636d5ff90b29ad211ffd79e16b65851539cdcca5
MarcosVillacanas <marcosvillacanas@icloud.com>
2020-03-17 10:43:40 +0100

    Useful Rooting Comment

--- backend/tenniShip/Angular/src/app/app-routing.module.ts
+++ backend/tenniShip/Angular/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [];
+//Aqui van las rutas de los componentes
+//Más tarde, a la hora de poner links, los links cambian el componente activo
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
