979cd250102cd17b018ee378f3a234739d613002
Diego <pascual.diego7@gmail.com>
2020-04-02 13:58:31 +0200

    Images Team Functional
    Co-Authored-By: ivanfernandezllorente <ivanfernandezllorente@users.noreply.github.com>
    Co-Authored-By: Álvaro Rivas <varo412@users.noreply.github.com>
    Co-Authored-By: marcosvillacanas <marcosvillacanas@icloud.com>
    Co-Authored-By: SantiagoGM <santiagognzlz@users.noreply.github.com>
    Co-Authored-By: diegopasfer1909 <diegopasfer1909@users.noreply.github.com>

--- frontend/Angular/src/app/service/image.service.ts
+++ frontend/Angular/src/app/service/image.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {catchError, map} from "rxjs/operators";
 import {Observable} from "rxjs";
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -12,11 +13,14 @@ export class ImageService {
 
   constructor(private  http : HttpClient) { }
 
-  public getTeamImage(teamName:string){
-    let url : string = this.urlFather + 'teams/'+teamName+'/image/0';
-    return this.http.get<File>(url).pipe(
-      map(response=>response),
-      catchError(error => Observable.throw("Server error (" + error.status + "): " + error.text()))
-    )
+  public getTeamImage(teamName:string,i : number):Observable<Blob>{
+    let url : string = this.urlFather + 'teams/'+teamName+'/image/'+i;
+    return this.http.get(url,{responseType:'blob'});
   }
+
+  public getTournamentImage(tournamentName:string):Observable<Blob>{
+    let url : string = this.urlFather + 'tournaments/'+tournamentName+'/image';
+    return this.http.get(url,{responseType:'blob'});
+  }
+
 }
