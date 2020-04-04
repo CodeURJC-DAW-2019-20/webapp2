0933d51f8d5c4b6056c588321891c7a060fa58f8
SantiagoGnzlz <santi.cervantes@hotmail.es>
2020-04-04 22:17:23 +0200

    Images work on Select Tournament!!

--- frontend/Angular/src/app/select-tournament/select-tournament.component.ts
+++ frontend/Angular/src/app/select-tournament/select-tournament.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Tournament} from "../model/tournament";
 import {PageLengthService} from "../service/page-length.service";
 import {SelectTournamentService} from "./select-tournament.service";
+import {ImageService} from "../service/image.service";
+import {SpinerService} from "../service/spiner.service";
 
 @Component({
   selector: 'app-select-tournament',
@@ -10,16 +12,26 @@ import {SelectTournamentService} from "./select-tournament.service";
 export class SelectTournamentComponent implements OnInit {
 
   private _tournamentList: Tournament[] = new Array<Tournament>();
+  private tournamentImages = [];
   private currentPage = 0;
   private pageSize = 2;   // Can be changed if we want
   public morePages: boolean = true;
 
-  constructor(private pagelength: PageLengthService, private selectTournamentService: SelectTournamentService) {}
+  constructor(private pagelength: PageLengthService, private selectTournamentService: SelectTournamentService,
+              private imageService: ImageService, private spinerService: SpinerService) {}
 
   ngOnInit(): void {
+    this.tournamentImages = new Array(this.pageSize);
     this.selectTournamentService.getPage(0,this.pageSize).subscribe(
       data =>{
         this._tournamentList = data;
+        for (let i=0; i < this.pageSize; i++) {
+          this.imageService.getTournamentImage(this._tournamentList[i].name).subscribe(
+            image=>{
+              this.createImageFromBlob(image,i);
+            },
+          );
+        }
       }
     );
     this.currentPage++;
@@ -40,6 +52,12 @@ export class SelectTournamentComponent implements OnInit {
           }
           else {
             this._tournamentList.push(aux[i]);
+            let realIndex = i+(this.pageSize*(this.currentPage-1));   // This might not work in all cases
+            this.imageService.getTournamentImage(this._tournamentList[realIndex].name).subscribe(
+              image=>{
+                this.createImageFromBlob(image,realIndex);
+              },
+            );
             i++;
           }
         }
@@ -49,5 +67,19 @@ export class SelectTournamentComponent implements OnInit {
     this.pagelength.updatePageLength();
   }
 
+  createImageFromBlob(image: Blob, i:number) {
+    let reader = new FileReader();
+    reader.addEventListener("load", () => {
+      this.tournamentImages[i] = reader.result;
+    }, false);
+
+    if (image) {
+      reader.readAsDataURL(image);
+    }
+  }
+
+  getTournamentImages(i: number) {
+    return this.tournamentImages[i];
+  }
 
 }
