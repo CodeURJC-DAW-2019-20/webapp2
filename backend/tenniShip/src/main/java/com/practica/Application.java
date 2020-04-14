9ec096493ee2e1beb3d49a15734c6fd1f9a72aaf
Diego <pascual.diego7@gmail.com>
2020-04-14 10:35:28 +0200

    Preparation for Docker

--- backend/tenniShip/src/main/java/com/practica/Application.java
+++ backend/tenniShip/src/main/java/com/practica/Application.java
@@ -5,11 +5,25 @@ import java.util.Properties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Application {
+	
+	@Configuration
+	public class CustomWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
+
+	    @Override
+	    public void addViewControllers(ViewControllerRegistry registry) {
+	        registry.addViewController("/new").setViewName("redirect:/new/");
+	        registry.addViewController("/new/").setViewName("forward:/new/index.html");
+	    super.addViewControllers(registry);
+	    }
+	}
 
 	public static void main(String[] args) {
 
--- frontend/Angular/src/app/app.component.ts
+++ frontend/Angular/src/app/app.component.ts
@@ -10,4 +10,5 @@ export class AppComponent {
 
   constructor(public spinerService: SpinnerService){}
 
+
 }
